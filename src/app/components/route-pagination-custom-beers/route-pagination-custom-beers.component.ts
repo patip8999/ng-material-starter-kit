c8b1163526004f40cfc58ca1c029d644adb99c04
patip8999 <patip8999@gmail.comm>
2023-01-15 17:19:05 +0100

    Route - Pagination - Frontend - Cities -  style

--- src/app/components/route-pagination-custom-beers/route-pagination-custom-beers.component.ts
+++ src/app/components/route-pagination-custom-beers/route-pagination-custom-beers.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
@@ -11,44 +15,51 @@ import { BeersService } from '../../services/beers.service';
   styleUrls: ['./route-pagination-custom-beers.component.scss'],
   templateUrl: './route-pagination-custom-beers.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RoutePaginationCustomBeersComponent {
-  readonly limits$: Observable<number[]> = of([5, 10, 15])
-  readonly page$: Observable<number[]> = of([1, 2, 3, 4, 5])
-  readonly queryParams$: Observable<{ pageNumber: number, pageSize: number }> =
+  readonly limits$: Observable<number[]> = of([5, 10, 15]);
+  readonly page$: Observable<number[]> = of([1, 2, 3, 4, 5]);
+  readonly queryParams$: Observable<{ pageNumber: number; pageSize: number }> =
     this._activatedRoute.queryParams.pipe(
       map((params) => ({
         pageNumber: !params['page'] || params['page'] < 0 ? 1 : +params['page'],
-        pageSize: !params['limit'] || params['limit'] < 0 ? 5 : +params['limit'],
+        pageSize:
+          !params['limit'] || params['limit'] < 0 ? 5 : +params['limit'],
       })),
       shareReplay(1),
       tap((data) => this.setFormValues(data.pageNumber, data.pageSize))
     );
 
-  readonly paginatorForm: FormGroup = new FormGroup({ limit: new FormControl(), page: new FormControl() });
+  readonly paginatorForm: FormGroup = new FormGroup({
+    limit: new FormControl(),
+    page: new FormControl(),
+  });
   readonly beers$: Observable<BeerModel[]> = this.queryParams$.pipe(
     switchMap((data) =>
-      this._beersService.getAllPagination(data.pageNumber, data.pageSize)));
+      this._beersService.getAllPagination(data.pageNumber, data.pageSize)
+    )
+  );
 
-  constructor(private _activatedRoute: ActivatedRoute, private _beersService: BeersService, private _router: Router) {
-  }
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _beersService: BeersService,
+    private _router: Router
+  ) {}
   ngAfterViewInit() {
     this.paginatorForm.valueChanges.subscribe((values) =>
       this._router.navigate([], {
         queryParams: {
           page: values.page,
-          limit: values.limit
-        }
+          limit: values.limit,
+        },
       })
     );
   }
   setFormValues(pageNumber: number, pageSize: number): void {
     this.paginatorForm.setValue({
       limit: pageSize,
-      page: pageNumber
-    })
+      page: pageNumber,
+    });
   }
-
-
 }
